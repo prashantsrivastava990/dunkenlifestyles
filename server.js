436de4916175835c53a3ff830f1bea863bfc99cb
Prashant Srivastava <prashantsrivastava551@gmail.com>
2025-07-20 23:41:49 +0530

    nn

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const port = process.env.PORT || 5000
 
 // ✅ MongoDB Connection
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/Ecom')
+mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://prashant:prashant1234@cluster0.4o4tznc.mongodb.net/Ecom')
   .then(() => console.log('✅ MongoDB Connected'))
   .catch((err) => console.error('❌ MongoDB Error:', err));
 
